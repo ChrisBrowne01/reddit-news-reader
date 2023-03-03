9a94a204cf29e34e62a3cf4841024ec517f6722a
Christopher Browne <christopher.browne@live.co.uk>
2023-03-03 23:42:16 +0000

    Add Header component to app

---
+++ reddit-news-reader/src/features/Header/Header.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchTerm } from '../../store/redditSlice';
+import logo from '../../logo.svg';
+//import '../Header/Header.css'
+
+
+const Header = () => {
+  const [searchTermPhrase, setSearchTermPhrase] = useState('');
+  const searchTerm = useSelector((state) => state.reddit.searchTerm);
+  const dispatch = useDispatch();
+
+  const onSearchTermChange = (e) => {
+    setSearchTermPhrase(e.target.value);
+  };
+
+  useEffect(() => {
+    setSearchTermPhrase(searchTerm);
+  }, [searchTerm]);
+
+  const onSearchTermSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setSearchTerm(searchTermPhrase));
+  };
+
+  return (
+    <div>
+      <div className="search-container">
+          <img className="reddit-logo" src={logo} alt="Reddit logo" />
+          <form onSubmit={onSearchTermSubmit} >
+              <span className='search-container'>
+                <input 
+                  id='search' 
+                  value={searchTermPhrase} 
+                  onChange={onSearchTermChange}
+                  type="text"
+                  placeholder="Search Reddit"
+                  aria-aria-label="Search post"
+                />
+              </span>
+             <button
+                className='search-button'
+                type='submit'
+                onClick={searchTermPhrase}
+                aria-aria-label="Search"
+              >
+                Search
+              </button>
+          </form>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
