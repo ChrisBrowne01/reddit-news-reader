6307fada9da36c9378d7869aa4741c4e3f1b855f
Christopher Browne <christopher.browne@live.co.uk>
2023-03-05 18:07:01 +0000

    Updated randomised Snoo avatar

--- reddit-news-reader/src/features/Avatar/Avatar.js
+++ reddit-news-reader/src/features/Avatar/Avatar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Avatar.css';
+import getRandomNumber from '../../utils/getRandomNumber';
 
 const Avatar = (props) => {
   const { name } = props;
 
+  // Generates randomised default placeholder Reddit Snoo avatar to display 
   return (
-    <img src={`https://api.adorable.io/avatars/10/${name}`}/* <-add a new fetch */
+    <img src={`https://www.redditstatic.com/avatars/defaults/v2/avatar_default_${getRandomNumber(1, 7)}.png`}
       alt={`${name} profile`}
       className="avatar-image"
     />
